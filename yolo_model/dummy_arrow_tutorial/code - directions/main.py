64c79073860fc1c7377fa43677e85aba3b82f770
Courtney Smith <courtney77smith77@gmail.com>
2024-01-18 10:11:12 -0600

    can identify directions in realtime with YOLO Struggles with right and left arrows

--- yolo_model/dummy_arrow_tutorial/code - directions/main.py
+++ yolo_model/dummy_arrow_tutorial/code - directions/main.py
@@ -7,11 +7,11 @@ model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 if torch.cuda.is_available():
     print('################ Trying to use cuda ##########')
-    device_name = torch.cuda.get_device_name(0)
+    # device_name = torch.cuda.get_device_name(0)
 
     print('################ found gpu ##########')
     # Use the model with cuda 
-    model.train(data="trainyaml.yaml", epochs=150, device=device_name)  # train the model
+    model.train(data="trainyaml.yaml", epochs=150, device=0)  # train the model
 
     print('################ input it here ##########')
 else :
